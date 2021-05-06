61af32cb5a6ee12c2e30632fbd1e20b333048c4d
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-05-07 05:10:33 +0900

    Initial Commit

---
+++ QiitaClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QiitaClient
+//
+//  Created by 秋月翔太 on 2021/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QiitaClient/QiitaClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaClientApp.swift
+//  QiitaClient
+//
+//  Created by 秋月翔太 on 2021/05/07.
+//
+
+import SwiftUI
+
+@main
+struct QiitaClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
